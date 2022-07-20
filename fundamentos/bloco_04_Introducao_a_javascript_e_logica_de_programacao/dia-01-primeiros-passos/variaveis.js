661413233db72108b608f0e4a5a05447f0869b50
Luana Portezan <luanaportezan@gmail.com>
2022-07-20 18:04:20 -0300

    variáveis

---
+++ fundamentos/bloco_04_Introducao_a_javascript_e_logica_de_programacao/dia-01-primeiros-passos/variaveis.js
@@ -0,0 +1,21 @@
+//valores das constantes e  variáveis
+const myName = "Luana";
+const birthCity = "Planaltina";
+let birthYear = 1989;
+
+//imprimir
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+//5.Altere o valor atribuído à variável birthYear para 2030. Faça um console.log(birthYear) novamente para ver o que acontece!
+
+//birthYear = 2030;
+//console.log(birthYear);
+
+//6.Altere o valor atribuído à constante birthCity. Faça um console.log(birthCity) novamente! Você saberia explicar por que recebemos uma mensagem de erro?
+
+//const birthCity = "Belém";
+//console.log(birthCity)
+
+
