40d5f926c8ca39ed48646fb05e5389078faf0686
Luana Portezan <luanaportezan@gmail.com>
2022-07-21 18:43:02 -0300

    resolução da questão 1

--- fundamentos/bloco_04_Introducao_a_javascript_e_logica_de_programacao/dia-02-array-e-loop-for/exercicios-01.js
+++ fundamentos/bloco_04_Introducao_a_javascript_e_logica_de_programacao/dia-02-array-e-loop-for/exercicios-01.js
@@ -13,3 +13,8 @@
 
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
+
+for (let index = 0; index < numbers.length; index += 1) {
+    const element = numbers[index];
+    console.log(element);
+}
