5e254e5957a75d168c701a41df23e8113c73cce4
Luana Portezan <luanaportezan@gmail.com>
2022-07-25 17:03:13 -0300

    resolução da parte 4

--- fundamentos/bloco_04_Introducao_a_javascript_e_logica_de_programacao/dia-04-objetos-e-funcoes/exercicios-objetos-e-for-in.js
+++ fundamentos/bloco_04_Introducao_a_javascript_e_logica_de_programacao/dia-04-objetos-e-funcoes/exercicios-objetos-e-for-in.js
@@ -40,7 +40,9 @@ for (let key in info) {
 // Sim
 //RESOLUÇÃO ABAIXO:
 
-
+for (let key in info) {
+  console.log(info[key])
+}
 
 // 5 - Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves. Valor esperado no console:
 // Margarida e Tio Patinhas
