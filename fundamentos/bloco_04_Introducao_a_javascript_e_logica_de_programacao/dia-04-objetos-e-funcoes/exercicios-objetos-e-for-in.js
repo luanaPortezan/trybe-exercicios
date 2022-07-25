636ca37ff19694c3d3b4e4da7f9b21772c098908
Luana Portezan <luanaportezan@gmail.com>
2022-07-25 16:15:37 -0300

    resolução 1 e inicio da parte 2

--- fundamentos/bloco_04_Introducao_a_javascript_e_logica_de_programacao/dia-04-objetos-e-funcoes/exercicios-objetos-e-for-in.js
+++ fundamentos/bloco_04_Introducao_a_javascript_e_logica_de_programacao/dia-04-objetos-e-funcoes/exercicios-objetos-e-for-in.js
@@ -1,5 +1,20 @@
 let info = {
-    personagem: 'Margarida',
-    origem: 'Pato Donald',
-    nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-  };
+  personagem: 'Margarida',
+  origem: 'Pato Donald',
+  nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+};
+
+//1 - Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome. Valor esperado no console:
+//Bem-vinda, Margarida
+//RESOLUÇÃO ABAIXO:
+//console.log("Bem vinda " + info.personagem);
+
+//2 - Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console. Valor esperado no console:
+//{
+//   personagem: 'Margarida',
+//   origem: 'Pato Donald',
+//   nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+//   recorrente: 'Sim'
+// }
+//RESOLUÇÃO ABAIXO:
+
