ec13409e6e26c7e8840c8c2f38e01980f331cee6
Luana Portezan <luanaportezan@gmail.com>
2022-07-25 16:45:07 -0300

    todas questões de objetos e for/in

--- fundamentos/bloco_04_Introducao_a_javascript_e_logica_de_programacao/dia-04-objetos-e-funcoes/exercicios-objetos-e-for-in.js
+++ fundamentos/bloco_04_Introducao_a_javascript_e_logica_de_programacao/dia-04-objetos-e-funcoes/exercicios-objetos-e-for-in.js
@@ -7,7 +7,8 @@ let info = {
 //1 - Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome. Valor esperado no console:
 //Bem-vinda, Margarida
 //RESOLUÇÃO ABAIXO:
-//console.log("Bem vinda " + info.personagem);
+
+console.log("Bem vinda " + info.personagem);
 
 //2 - Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console. Valor esperado no console:
 //{
@@ -17,4 +18,71 @@ let info = {
 //   recorrente: 'Sim'
 // }
 //RESOLUÇÃO ABAIXO:
+
+info.recorrente = "sim";
+console.log(info);
+
+//3 - Faça um for/in que mostre todas as chaves do objeto. Valor esperado no console:
+// Copiar
+// personagem
+// origem
+// nota
+// recorrente
+//RESOLUÇÃO ABAIXO:
+
+
+
+// 4 - Faça um novo for/in, mas agora mostre todos os valores das chaves do objeto. Valor esperado no console:
+// Copiar
+// Margarida
+// Pato Donald
+// Namorada do personagem principal nos quadrinhos do Pato Donald
+// Sim
+//RESOLUÇÃO ABAIXO:
+
+
+
+// 5 - Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves. Valor esperado no console:
+// Copiar
+// Margarida e Tio Patinhas
+// Pato Donald e Christmas on Bear Mountain, Dell's Four Color Comics #178
+// Namorada do personagem principal nos quadrinhos do Pato Donald e O último MacPatinhas
+// Ambos recorrentes // Atenção para essa última linha!
+// Usando o objeto abaixo, faça os exercícios a seguir:
+// Copiar
+// let leitor = {
+//   nome: 'Julia',
+//   sobrenome: 'Pessoa',
+//   idade: 21,
+//   livrosFavoritos: [
+//     {
+//       titulo: 'O Pior Dia de Todos',
+//       autor: 'Daniela Kopsch',
+//       editora: 'Tordesilhas',
+//     },
+//   ],
+// };
+//RESOLUÇÃO ABAIXO:
+
+
+
+// 🚀 6 - Acesse as chaves nome, sobrenome e titulo, que está dentro da chave livrosFavoritos, e faça um console.log no seguinte formato: "O livro favorito de Julia Pessoa se chama 'O Pior Dia de Todos'".
+//RESOLUÇÃO ABAIXO:
+
+
+
+// 🚀 7 - Adicione um novo livro favorito na chave livrosFavoritos, que é um array. Atribua a esta chave um objeto contendo as seguintes informações:
+// Copiar
+// {
+//   titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+//   autor: 'JK Rowling',
+//   editora: 'Rocco',
+// }
+//RESOLUÇÃO ABAIXO:
+
+
+
+// 🚀 8 - Acesse as chaves nome e livrosFavoritos e faça um console.log no seguinte formato: "Julia tem 2 livros favoritos".
+//RESOLUÇÃO ABAIXO:
+
 
