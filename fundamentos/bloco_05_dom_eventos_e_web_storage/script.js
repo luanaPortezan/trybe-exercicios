c7ee57f1744f2a913408c2089578d5450967ee16
Luana Portezan <luanaportezan@gmail.com>
2022-07-28 00:57:29 -0300

    requisito 2 concluído - Crie e execute uma função que mude a cor do quadrado amarelo para o verde da Trybe (rgb(76,164,109)).

--- fundamentos/bloco_05_dom_eventos_e_web_storage/script.js
+++ fundamentos/bloco_05_dom_eventos_e_web_storage/script.js
@@ -3,3 +3,9 @@ function changeText() {
   paragraph.innerHTML = "A porta de entrada para sua carreira em tecnologia";
 };
 changeText();
+
+function changeSquareToGreen() {
+  let squareYellow = document.getElementsByClassName('main-content')[0];
+  squareYellow.style.background = "rgb(76,164,109)";
+}
+changeSquareToGreen();
