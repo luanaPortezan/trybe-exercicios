500b5bdd1932ad2acac21c257c34795940d9ff5e
Luana Portezan <luanaportezan@gmail.com>
2022-07-28 01:45:47 -0300

    Requisito 5 concluído

--- fundamentos/bloco_05_dom_eventos_e_web_storage/script.js
+++ fundamentos/bloco_05_dom_eventos_e_web_storage/script.js
@@ -21,13 +21,19 @@ changeSquareToWhite();
 
 // Requisito 4 - Crie e execute uma função que corrija o texto da tag <h1>.
 function changeNameTitle() {
-  let nameCorrectin = document.getElementsByClassName('title')[0];
-  nameCorrectin.innerHTML = "Exercício 5.1 - JavaScript";
+  let nameCorrection = document.getElementsByClassName('title')[0];
+  nameCorrection.innerHTML = "Exercício 5.1 - JavaScript";
 }
 changeNameTitle();
 
-
 // Requisito 5 - Crie e execute uma função que modifique todo o texto da tag <p> para maiúsculo.
+function changeToUpperCase() {
+  let paragraphAll = document.getElementsByTagName('p');
 
+  for (let index = 0; index < paragraphAll.length; index += 1) {
+    paragraphAll[index].innerHTML = paragraphAll[index].innerHTML.toUpperCase();
+  }
+};
+changeToUpperCase();
 
 // Requisito 6 - Crie e execute uma função que exiba o conteúdo de todas as tags <p> no console.
