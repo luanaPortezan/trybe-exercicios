f9e74faef0dca48f40a0afa2c3bc80e3bcc62301
Luana Portezan <luanaportezan@gmail.com>
2022-08-05 18:00:44 -0300

    um botão que limpe as informações contidas nos campo

--- fundamentos/bloco_06_forms_flexbox_e_responsivo/dia-01-forms/script.js
+++ fundamentos/bloco_06_forms_flexbox_e_responsivo/dia-01-forms/script.js
@@ -6,3 +6,15 @@ window.onload = function () {
   const submitBtn = document.querySelector('#submit-btn');
   submitBtn.addEventListener('click', handleSubmit);
 };
+
+function clearFields() {
+    const formElements = document.querySelectorAll('input');
+    const textArea = document.querySelector('textarea');
+    for (let index = 0; index < formElements.length; index += 1) {
+      const userInput = formElements[index];
+      userInput.value = '';
+      userInput.checked = false;
+    }
+    textArea.value = '';
+  }
+  
