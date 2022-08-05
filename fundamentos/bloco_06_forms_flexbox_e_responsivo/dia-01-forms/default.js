12a0b0546f783957e0879186269f29978545642b
Luana Portezan <luanaportezan@gmail.com>
2022-08-05 17:56:28 -0300

    estrutura finalizada

--- fundamentos/bloco_06_forms_flexbox_e_responsivo/dia-01-forms/default.js
+++ fundamentos/bloco_06_forms_flexbox_e_responsivo/dia-01-forms/default.js
@@ -7,7 +7,7 @@ const HREF_LINK = document.querySelector("#href");
 // 3 - Adicione um evento keypress no elemento input type="text" que possui o id input-text. Na função do evento, implemente o método .preventDefault() de forma que somente o caractere a (letra 'a' minúscula) possa ser digitado na caixinha. (dica: para capturar a tecla digitada, utilize o event.key)
 
 HREF_LINK.addEventListener("click", (event) => {
-  console.log(event);
+  // console.log(event);
   event.preventDefault();
 
 });
