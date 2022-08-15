8849875c2e48a1183eb7419e901644cd6aa79685
Luana Portezan <luanaportezan@gmail.com>
2022-08-15 16:36:41 -0300

    part 2 - objetct.keys1

--- fundamentos/bloco_07_ES6_testes_unitarios/part2.js
+++ fundamentos/bloco_07_ES6_testes_unitarios/part2.js
@@ -1,8 +1,28 @@
+// const coolestTvShow = {
+//   name: "BoJack Horseman",
+//   genre: "adult animation",
+//   createdBy: "Raphael Bob-Waksberg",
+//   favoriteCharacter: "Princess Carolyn",
+//   quote: "Princess Carolyn always lands on her feet.",
+//   seasons: 6,
+// };
+
 const coolestTvShow = {
   name: "BoJack Horseman",
   genre: "adult animation",
-  createdBy: "Raphael Bob-Waksberg",
+  author: "Raphael Bob-Waksberg",
   favoriteCharacter: "Princess Carolyn",
   quote: "Princess Carolyn always lands on her feet.",
   seasons: 6,
 };
+
+for (const property in coolestTvShow) {
+  console.log(property);
+}
+
+// name
+// genre
+// author
+// favoriteCharacter
+// quote
+// seasons
