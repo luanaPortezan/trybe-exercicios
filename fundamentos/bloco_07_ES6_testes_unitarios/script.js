3aa64212060f22167b4f87c4cfb51edc9a5eaf09
Luana Portezan <luanaportezan@gmail.com>
2022-08-15 16:17:08 -0300

    usando typeof

--- fundamentos/bloco_07_ES6_testes_unitarios/script.js
+++ fundamentos/bloco_07_ES6_testes_unitarios/script.js
@@ -3,5 +3,13 @@
 // const sum = (value1, value2) => value1 + value2;
 // console.log(sum(2, 3));
 
-const sum = (value1, value2) => value1 + value2;
-console.log(sum(2, '3')); // resultado: 23
+// const sum = (value1, value2) => value1 + value2;
+// console.log(sum(2, '3')); // resultado: 23
+
+const sum = (value1, value2) => {
+  if (typeof value1 !== 'number' || typeof value2 !== 'number') {
+    return 'Os valores devem ser numéricos';
+  }
+  return value1 + value2;
+};
+console.log(sum(2, '3'));
