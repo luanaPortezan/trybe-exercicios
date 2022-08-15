946cbba74a7382ecfa0511c7106ced7df7207673
Luana Portezan <luanaportezan@gmail.com>
2022-08-15 16:18:28 -0300

    usando throw new error

--- fundamentos/bloco_07_ES6_testes_unitarios/script.js
+++ fundamentos/bloco_07_ES6_testes_unitarios/script.js
@@ -6,9 +6,17 @@
 // const sum = (value1, value2) => value1 + value2;
 // console.log(sum(2, '3')); // resultado: 23
 
+// const sum = (value1, value2) => {
+//   if (typeof value1 !== 'number' || typeof value2 !== 'number') {
+//     return 'Os valores devem ser numéricos';
+//   }
+//   return value1 + value2;
+// };
+// console.log(sum(2, '3'));
+
 const sum = (value1, value2) => {
   if (typeof value1 !== 'number' || typeof value2 !== 'number') {
-    return 'Os valores devem ser numéricos';
+    throw new Error('Os valores devem ser numéricos');
   }
   return value1 + value2;
 };
