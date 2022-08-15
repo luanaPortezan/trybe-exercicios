dbd71d77d2559cb9bc21407556e191947b71327e
Luana Portezan <luanaportezan@gmail.com>
2022-08-15 16:24:28 -0300

    part 1 - chaves2

--- fundamentos/bloco_07_ES6_testes_unitarios/part1.js
+++ fundamentos/bloco_07_ES6_testes_unitarios/part1.js
@@ -1,5 +1,12 @@
+// const customer = {
+//   firstName: 'Roberto',
+//   age: 22,
+//   job: 'Teacher',
+// };
+
 const customer = {
   firstName: 'Roberto',
+  lastName: 'Faria', // Propriedade adicionada.
   age: 22,
   job: 'Teacher',
 };
