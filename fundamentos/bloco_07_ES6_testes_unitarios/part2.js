ee0980055d4ddd61c24c5d1ceaf660982b6c7c25
Luana Portezan <luanaportezan@gmail.com>
2022-08-15 16:35:17 -0300

    part 2 - objetct.keys

---
+++ fundamentos/bloco_07_ES6_testes_unitarios/part2.js
@@ -0,0 +1,8 @@
+const coolestTvShow = {
+  name: "BoJack Horseman",
+  genre: "adult animation",
+  createdBy: "Raphael Bob-Waksberg",
+  favoriteCharacter: "Princess Carolyn",
+  quote: "Princess Carolyn always lands on her feet.",
+  seasons: 6,
+};
