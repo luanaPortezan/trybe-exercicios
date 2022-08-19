92efc63d968f0114aaab5bebdb1215f9e04b5ff7
Luana Portezan <luanaportezan@gmail.com>
2022-08-19 17:06:08 -0300

    resolução da fixaçaõ HOF

--- fundamentos/bloco_08_higher_order_functions_ES6/dia-1-introducao/fixacao.js
+++ fundamentos/bloco_08_higher_order_functions_ES6/dia-1-introducao/fixacao.js
@@ -10,3 +10,30 @@
 
 // Ela deve retornar o valor do respectivo parâmetro, neste caso:
 // Acordando!!
+
+// Primeiro Passo
+// Vamos criar nossas funções com arrow functions.
+
+// const wakeUp = () => 'Acordando!!';
+
+// const breakfast = () => 'Bora tomar café!!';
+
+// const sleep = () => 'Partiu dormir!!';
+
+// Segundo Passo
+// Agora vamos criar nossa HOF, ela receberá por parâmetro uma das funções que criamos e então irá imprimir a mensagem gerada pela HOF. Atribuiremos a execução de nossa callback a uma constante dentro do escopo da HOF e então imprimimos essa constante.
+
+const wakeUp = () => 'Acordando!!';
+
+const breakfast = () => 'Bora tomar café!!';
+
+const sleep = () => 'Partiu dormir!!';
+
+const doingThings = (callback) => {
+  const result = callback();
+  console.log(result);
+};
+
+doingThings(wakeUp); // Acordando!!
+doingThings(breakfast); // Bora tomar café!!
+doingThings(sleep); // Partiu dormir!!
